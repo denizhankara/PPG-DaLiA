4f154ca5cf13236fd8967f16bc2a113631a90d66
denizhankara <denizhankara@ku.edu.tr>
2021-04-08 15:33:07 +0300

    Initial commit for setting up repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='PPG Dalia dataset HR estimation',
+    author='UIUC-teamDL',
+    license='MIT',
+)
